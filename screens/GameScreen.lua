fabe0704055188d1e03b4dd96bfa56dc4c45f965
Matheus Rossi Saciotto <saciotto@gmail.com>
2019-12-24 22:01:24 -0300

    Adiciona mapas de fases

--- screens/GameScreen.lua
+++ screens/GameScreen.lua
@@ -11,15 +11,55 @@ local Pad = require("entities.Pad")
 
 local GameScreen = Screen:new()
 
+local function getColor(c)
+    if c == 'b' or c == 'B' then
+        return "blue"
+    elseif c == 'g' or c == 'G' then
+        return 'green'
+    elseif c == 'o' or c == 'O' then
+        return 'orange'
+    elseif c == 'w' or c == 'W' then
+        return 'white'
+    elseif c == 'y' or c == 'Y' then
+        return 'yellow'
+    else
+        return 'white'
+    end
+end
+
+local function createBlock(c, i, j, lenght)
+    local color = getColor(c)
+    local brick = Brick:new(Constants.BRICK_WIDTH * j, Constants.BRICK_HEIGHT * i, color, lenght)
+    return brick
+end
+
+local function readMap(self)
+    local filename = "./assets/maps/stage1.txt"
+    local i = 0
+    for line in io.lines(filename) do
+        local c = '.'
+        local j1, j2 = 0
+        for j = 1, line:len(), 1 do
+            if c ~= '.' then
+                if c ~= line:sub(j,j) then
+                    j2 = j - 1
+                    local brick = createBlock(c, i, j1, j2 - j1)
+                    table.insert(self.bricks, brick)
+                    c = '.'
+                end
+            end
+            if c == '.' and line:sub(j,j) ~= '.' then
+                c = line:sub(j,j)
+                j1 = j - 1
+            end
+        end
+        i = i + 1
+    end
+end
+
 local function createBlocks(self)
     self.bricks = {}
-    local lenght = 3
-    for i = 2, Constants.NO_ROWS, 1 do
-        for j = 1, Constants.NO_COLS , 1 do
-            local brick = Brick:new(Constants.BRICK_WIDTH * j * lenght, Constants.BRICK_HEIGHT * i, "blue", lenght)
-            table.insert(self.bricks, brick)
-        end
-    end
+    readMap(self)
 end
 
 function GameScreen:new()
