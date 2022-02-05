b0c6dffd63871d95aca235cb043e12e89cb406b9
Matheus Rossi Saciotto <saciotto@gmail.com>
2022-02-05 12:41:10 -0300

    Corrige nome da font DejaVu

--- game/Fonts.lua
+++ game/Fonts.lua
@@ -1,5 +1,5 @@
 local Fonts = {
-    DEJAVU = "assets/fonts/DejavuSans.ttf",
+    DEJAVU = "assets/fonts/DejaVuSans.ttf",
     CHILANKA = "assets/fonts/Chilanka-Regular.otf",
     TOONEY_NOODLE = "assets/fonts/TooneyNoodleNF.otf",
     MATERIAL_ICONS = "assets/fonts/Material-Icons.ttf"
